2bc4dfcb05df668d49944d448d8c687061382159
Rob Hird <robertjhird@gmail.com>
2024-11-13 20:39:00 -0500

    main renamed go_contract to go_config

--- generate.go
+++ generate.go
@@ -2,4 +2,4 @@ package betterreads
 
 // install OpenAPI codegen with `go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest`
 
-//go:generate oapi-codegen --config=./go_contract/config.yaml ./betterreads.yaml
+//go:generate oapi-codegen --config=./go_config/config.yaml ./betterreads.yaml
