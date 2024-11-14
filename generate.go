099a0db275ba19b746b20f537b8f7fd5980904c2
Rob Hird <robertjhird@gmail.com>
2024-11-13 20:53:24 -0500

    main updated so you do not need to install the tool

--- generate.go
+++ generate.go
@@ -2,4 +2,4 @@ package betterreads
 
 // install OpenAPI codegen with `go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest`
 
-//go:generate oapi-codegen --config=./go_config/config.yaml ./betterreads.yaml
+//go:generate go run -mod=mod github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest --config=./go_config/config.yaml ./betterreads.yaml
