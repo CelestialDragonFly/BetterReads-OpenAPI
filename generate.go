8f0dca7cdb65937b4ae31dee75c1f74ccbfb1e08
Rob Hird <robertjhird@gmail.com>
2024-11-13 20:38:04 -0500

    main added go codegen

---
+++ generate.go
@@ -0,0 +1,5 @@
+package betterreads
+
+// install OpenAPI codegen with `go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest`
+
+//go:generate oapi-codegen --config=./go_contract/config.yaml ./betterreads.yaml
