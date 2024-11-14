1f55b4488d2e446195e5f7f43cf1a59b14d7efbd
Rob Hird <robertjhird@gmail.com>
2024-11-13 20:54:00 -0500

    main removed comment

--- generate.go
+++ generate.go
@@ -1,5 +1,3 @@
 package betterreads
 
-// install OpenAPI codegen with `go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest`
-
 //go:generate go run -mod=mod github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest --config=./go_config/config.yaml ./betterreads.yaml
